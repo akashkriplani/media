148ce762ef45cf0fc1d805c785f04f467d708856
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-09 16:36:35 +0530

    Create a reusable custom thunk hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,29 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
+import { useThunk } from '../hooks/use-thunk';
 
 function UsersList() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUserError, setCreatingUserError] = useState(null);
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
   const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    setIsLoadingUsers(true);
-    // dispatch returns a promise which does not work like a JS promise
-    // It goes to the then() block on both success and failure
-    // To achieve what a classic promise does we use unwrap() to convert it to
-    // a classic JS promise
-    dispatch(fetchUsers())
-      .unwrap()
-      .catch((err) => setLoadingUsersError(err))
-      .finally(() => setIsLoadingUsers(false));
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
@@ -34,15 +24,7 @@ function UsersList() {
   }
 
   const handleUserAdd = () => {
-    // dispatch returns a promise which does not work like a JS promise
-    // It goes to the then() block on both success and failure
-    // To achieve what a classic promise does we use unwrap() to convert it to
-    // a classic JS promise
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    doCreateUser();
   };
 
   const renderedUsers = data.map((user) => {
