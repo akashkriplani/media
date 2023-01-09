c8bfbd83a4d5b29b66e362de29d9ea57f037c77a
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-09 14:04:01 +0530

    Set the loading state from the component state and remove it from the redux state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,23 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+
   const dispatch = useDispatch();
 
-  const { isLoading, data, error } = useSelector((state) => state.users);
+  const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    // dispatch returns a promise which does not work like a JS promise
+    // It goes to the then() block on both success and failure
+    // To achieve what a classic promise does we use unwrap() to convert it to
+    // a classic JS promise
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => setLoadingUsersError(err))
+      .finally(() => setIsLoadingUsers(false));
   }, [dispatch]);
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
