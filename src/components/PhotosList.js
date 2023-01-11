96249c83d816be3dc052589cf82a2c13b7bcdc83
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-11 12:48:37 +0530

    Add invalidateTags to enable re-fetching of data

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -27,7 +27,7 @@ function PhotosList({ album }) {
     <div>
       <div className="m-2 flex flex-row items-center justify-between">
         <h3 className="text-lg font-bold">Photos in {album.title}</h3>
-        <Button onLoading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
           + Add Photo
         </Button>
       </div>
--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,18 +1,22 @@
-import { GoTrashcan } from 'react-icons/go';
+import { GoTrashcan, GoSync } from 'react-icons/go';
 import { useRemovePhotoMutation } from '../store';
 
 function PhotosListItem({ photo }) {
-  const [removePhoto] = useRemovePhotoMutation();
+  const [removePhoto, results] = useRemovePhotoMutation();
 
   const handleRemovePhoto = () => {
-    removePhoto(photo);
+    // While the photo is being removed, prevent the user from clicking on the deleted photo
+    // again. Hence, the loading flag is used over here, to prevent duplicate hits to the server
+    if (!results.isLoading) {
+      removePhoto(photo);
+    }
   };
 
   return (
     <div onClick={handleRemovePhoto} className="relative cursor-pointer m-2">
       <img className="h-20 w-20" src={photo.url} alt="random pic" />
       <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
-        <GoTrashcan className="text-3xl" />
+        {results.isLoading ? <GoSync className="animate-spin" /> : <GoTrashcan className="text-3xl" />}
       </div>
     </div>
   );
